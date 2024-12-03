a9b18d9f5f5aed848dd2a7dc495592e9bf8d44cf
svasim <svante.ericsson06@gmail.com>
2024-12-03 15:59:39 +0100

    Fixed environment variables not loading in models.py

--- models.py
+++ models.py
@@ -2,6 +2,9 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker, declarative_base
 from passlib.context import CryptContext
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
